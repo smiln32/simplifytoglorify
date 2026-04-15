a05295027b99f4cd30418c2db59ec2a8dac304a0
smiln32 <57502523+smiln32@users.noreply.github.com>
2026-04-15 17:56:50 -0500

    Add routing for Blog page in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import Blog from './pages/blog.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
