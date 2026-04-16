2ed03c9baaaac9c6ecf045d5fafdc1a717281940
smiln32 <57502523+smiln32@users.noreply.github.com>
2026-04-15 19:03:44 -0500

    Add route for BlogPost component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import Blog from './pages/blog.tsx'
+import BlogPost from './BlogPosts.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
